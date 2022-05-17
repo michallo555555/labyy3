6d23e5b63e746dbb46cc108792b89add7dcf66be
michallo555555 <101186827+michallo555555@users.noreply.github.com>
2022-05-17 07:06:00 +0000

    podstawowy kod

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
+import java.io.IOException;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    try {
+      Service1 s = new Service1();
+      s.addStudent(new Student("Krzysztof", 20));
+      s.addStudent(new Student("Janusz", 40));
+
+      var students = s.getStudents();
+      for(Student current : students) {
+        System.out.println(current.ToString());
+      }
+    } catch (IOException e) {
+
+    }
   }
 }
